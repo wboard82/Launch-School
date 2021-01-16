026619cbed746bb89a05ea6fa1d0e596c5381800
Joshua Keller <jrkeller@gmail.com>
2021-01-15 22:32:12 -0500

    Initial commit of Twenty-One

---
+++ rb_120/05_slightly_larger/twenty_one/twenty_one.rb
@@ -0,0 +1,297 @@
+=begin
+Design choices:
+- Separate out the "playing" aspects of the dealer from the "dealing" aspects
+- Hand is a separate class that each player has one of
+- Deck does not deal, the thing you can call on a Deck is to draw_card
+- Game is really the coordinator, takes care of the dealing and taking turns
+- This way the player and the deck are unaware of each other
+-
+
+=end
+class Card
+  attr_reader :rank, :suit
+
+  BACK = """
+┌───────┐
+│░░░░░░░│
+│░░░░░░░│
+│░░░░░░░│
+│░░░░░░░│
+│░░░░░░░│
+└───────┘
+"""
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def value
+    case rank
+    when :A then 11
+    when :K, :Q, :J then 10
+    else rank
+    end
+  end
+
+  def symbol
+    case suit
+    when :Spades then '♠'
+    when :Diamonds then '♦'
+    when :Hearts then '♥'
+    when :Clubs then '♣'
+    end
+  end
+
+  def to_s
+    """
+┌───────┐
+|#{rank.to_s.ljust(7)}|
+|       |
+|#{symbol.center(7)}|
+|       |
+|#{rank.to_s.rjust(7)}|
+└───────┘
+ """
+  end
+end
+
+class Deck
+  RANKS = [:A, :K, :Q, :J, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+  SUITS = [:Hearts, :Diamonds, :Clubs, :Spades]
+
+  def initialize
+    @deck = []
+    RANKS.each do |rank|
+      SUITS.each do |suit|
+        @deck << Card.new(rank, suit)
+      end
+    end
+    @deck.shuffle!
+  end
+
+  def draw_card
+    @deck.pop
+  end
+
+  #!!! Delete this before submitting
+  def display_deck
+    puts "#{@deck.size} cards in the deck:"
+    @deck.each { |card| puts card }
+  end
+end
+
+class Hand
+  attr_reader :hand
+
+  def initialize
+    @hand = []
+  end
+
+  def total
+    aces_count = @hand.count { |card| card.rank == :Ace }
+    total = @hand.inject(0) { |sum, card| sum + card.value }
+
+    while total > 21 && aces_count > 0
+      total -= 10
+      aces_count -= 1
+    end
+
+    total
+  end
+
+  def <<(card)
+    @hand << card
+  end
+
+  def list_cards
+    @hand.map(&:to_s)
+  end
+
+  def display(hide_card: false)
+    if hide_card
+      split_cards = [hand.first.to_s.split("\n"), Card::BACK.split("\n")]
+    else
+      split_cards = hand.map { |card| card.to_s.split("\n") }
+    end
+
+    0.upto(7) do |idx|
+      puts split_cards.map{ |card| card[idx] }.join("  ")
+    end
+  end
+end
+
+class Player
+  attr_reader :hand, :name
+
+  def initialize(name)
+    @name = name
+    @hand = Hand.new
+  end
+
+  def busted?
+    hand.total > 21
+  end
+
+  def take_turn
+    loop do
+      answer = decide
+      hit if answer == 'h'
+      break if answer == 's'
+    end
+  end
+
+  def display_hand
+    puts "#{name} has:"
+    hand.display
+  end
+
+  def score
+    hand.total
+  end
+
+  def display_score
+    puts "#{name} has #{hand.total}"
+  end
+
+  def <<(card)
+    hand << card
+  end
+
+  def >(other)
+    hand.total > other.hand.total
+  end
+end
+
+class Human < Player
+  def initialize
+    super("Human")
+  end
+
+  def hit?
+    puts "You have #{score}."
+    puts "Would you like to (h)it or (s)tay?"
+    answer = nil
+    loop do
+      answer = gets.chomp.downcase
+      break if %w(h s hit stay).include?(answer)
+      puts "Please enter 'h' or 's'."
+    end
+    answer[0] == 'h'
+  end
+end
+
+class Dealer < Player
+  attr_reader :hide_card
+
+  def initialize
+    @hide_card = true
+    super("Dealer")
+  end
+
+  def flip_card
+    @hide_card = false
+  end
+
+  def hit?
+    hand.total <= 16
+  end
+
+  def display_hand
+    puts "#{name} has:"
+    hand.display(hide_card: hide_card)
+  end
+end
+
+class Game
+  attr_reader :deck, :human, :dealer, :current_player
+
+  def initialize
+    @deck = Deck.new
+    @human = Human.new
+    @dealer = Dealer.new
+    @current_player = human
+  end
+
+  def play
+    display_welcome
+    deal_initial_cards
+    current_player_turn
+    next_player
+    current_player_turn unless human.busted?
+    display_result
+  end
+
+  def clear_screen
+    system('clear') || system('cls')
+  end
+
+  def display_welcome
+    clear_screen
+    puts "*****************************"
+    puts "*** Welcome to Twenty-One ***"
+    puts "*****************************"
+  end
+
+  def next_player
+    @current_player = dealer
+    dealer.flip_card if @current_player == dealer
+  end
+
+  def deal_initial_cards
+    2.times do
+      human << deck.draw_card
+      dealer << deck.draw_card
+    end
+  end
+
+  def display_cards
+    dealer.display_hand
+    puts
+    human.display_hand
+    puts
+  end
+
+  def current_player_turn
+    loop do
+      clear_screen
+      display_cards
+
+      if current_player.hit?
+        puts "#{current_player.name} chose to hit on #{current_player.score}"
+        sleep 1
+        card = deck.draw_card
+        puts "The card is: #{card}"
+        current_player << card
+        sleep 1.5
+      else
+        puts "#{current_player.name} chose to stay on #{current_player.score}"
+        break
+        sleep 1.5
+      end
+
+      if current_player.busted?
+        puts "#{current_player.name.upcase} BUSTED!"
+        break
+      end
+    end
+  end
+
+  def display_result
+    display_cards
+    display_winner
+    puts
+  end
+
+  def display_winner
+    if human.busted? || dealer > human
+      puts "#{dealer.name} wins."
+    elsif dealer.busted? || human > dealer
+      puts "#{human.name} won!"
+    else
+      puts "It's a push!"
+    end
+  end
+end
+
+Game.new.play
